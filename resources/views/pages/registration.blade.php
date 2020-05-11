0373d6e16edfcf11578bb3173d050f56b506d823
Bradley Juma <bradleyjuma170@gmail.com>
2020-05-11 21:01:16 +0300

    clean up

--- resources/views/pages/registration.blade.php
+++ resources/views/pages/registration.blade.php
@@ -3,8 +3,48 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="stylesheet" href="{{('css/app.css')}}">
 
+<style>
+.divider-text {
+    position: relative;
+    text-align: center;
+    margin-top: 15px;
+    margin-bottom: 15px;
+}
+.divider-text span {
+    padding: 7px;
+    font-size: 12px;
+    position: relative;
+    z-index: 2;
+}
+.divider-text:after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    border-bottom: 1px solid #ddd;
+    top: 55%;
+    left: 0;
+    z-index: 1;
+}
+
+.btn-facebook {
+    background-color: #405D9D;
+    color: #fff;
+}
+.btn-twitter {
+    background-color: #42AEEC;
+    color: #fff;
+}
+
+
+
+
+
+
+
+</style>
 
     <title>Registration</title>
   </head>
@@ -43,7 +83,7 @@
       		</div>
           	<input name="" class="form-control" placeholder="Phone number" type="text">
           </div> <!-- form-group// -->
-          
+
           <div class="form-group input-group">
           	<div class="input-group-prepend">
       		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
